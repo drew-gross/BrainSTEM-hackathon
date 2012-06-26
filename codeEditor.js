d0eb38bfd9f2252806b9919f29d353d061dc84da
Drew Gross <drew.a.gross@gmail.com>
2012-06-25 20:34:32 -0400

    return inputs in UserCode.inputs

--- codeEditor.js
+++ codeEditor.js
@@ -1,4 +1,28 @@
 $(function () {
+    //helper functions
+    var inputs = function() {
+        elems = $(".input").filter(function () {
+            var partLocation = $(this).data("location");
+            if (typeof(partLocation) !== "string") {
+                return false;
+            }
+            return (partLocation.search("robot") === 0);
+        });
+        return _.map(elems,function (elem, key) {
+            var indexRegex = /robot-(\d+)/g;
+            var matches = indexRegex.exec($(elem).data("location"));
+            var index = matches[1];
+            return {
+                        sensor:{},
+                        position:
+                        {
+                            x:index % 4,
+                            y:Math.floor(index / 4)
+                        }
+                   };
+                   
+        });
+    };
     //set up the code editor
     editAreaLoader.init({
         id: "usercode"		// textarea id
@@ -7,6 +31,8 @@ $(function () {
     });
     window.UserCode = {};
     UserCode.code = "";
+    UserCode.inputs = [];
+    UserCode.outputs = [];
     UserCode.run = function(inputs){
         var outputs = {};
         eval(UserCode.code);
@@ -15,6 +41,7 @@ $(function () {
 
     $("#run").click(function () {
         UserCode.code = editAreaLoader.getValue("usercode");
+        UserCode.inputs = inputs();
         Game.running = true;
     });
     //set up the robot editor
@@ -22,6 +49,11 @@ $(function () {
         drop(ev);
     });
     $(".robot-part").draggable({ revert: "invalid", snap: ".robot-cell, .tool-box-cell", snapMode: "inner" });
-    $(".robot-cell").droppable({ accept: ".robot-part" });
+    $(".robot-cell").droppable({ 
+        accept: ".robot-part",
+        drop: function (event, ui) {
+            ui.draggable.data("location", this.id);
+        }
+    });
     $(".tool-box-cell").droppable({ accept: ".robot-part" });
 });
